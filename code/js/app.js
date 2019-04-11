fc6d5372ece86a919b591d76d0ced5cb46ffcc2c
alessiodotti15 <alessio2915@gmail.com>
2019-04-11 12:03:34 +0200

    update page

--- code/js/app.js
+++ code/js/app.js
@@ -1,15 +1,33 @@
 var app = {
-	init: function() {
+	init: function () {
 		console.log("init");
 		$.getJSON("api/data.json")
-		.done(app.showCards)
-		.fail(app.onError);
+			.done(app.showCards)
+			.fail(app.onError);
 	},
 	showCards(dataJson) {
 		console.log(dataJson);
-		
+
+		dataJson.cardlist.forEach(element => {
+			let card = `<div class="col-lg-3 col-md-6 mb-4 net-int">
+								<div class="card h-100">
+								<img class="card-img-top" src="${element.imgUrl}" alt="CISCO Sim Desktop">
+								<div class="card-body">
+									<h4 class="card-title">${element.title}</h4>
+									<p class="card-text">${element.desc}</p>
+								</div>
+								<div class="card-footer">
+									<a href="${element.buttonUrl}" class="btn btn-primary">Accedi</a>
+								</div>
+								</div>
+							</div>`;
+			$("#card-container").append(card);
+		});
+
 	},
-	onError: function(e) {
+
+
+	onError: function (e) {
 		console.log(`Ajax error: ${JSON.stringify(e)}`);
 	}
 
